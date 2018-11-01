dca45606f90c403350d4623a5d4dd7c3a5fdf0cb
Wasif Khan <waasif.khaan@gmail.com>
2018-11-01 00:50:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Wasif Khan';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.first}!`
